42a348e38f086b389d4d4733910b4d7b1f8d0ac7
Matt <MattP598@gmail.com>
2022-05-05 08:00:56 -0500

    ClassesExercise1

---
+++ ClassesExercise/Car.cs
@@ -0,0 +1,11 @@
+﻿namespace ClassesExercise1
+{
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Year { get; set; }
+
+        
+    }
+}
---
+++ ClassesExercise/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ClassesExercise1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var myCar = new Car();
+
+            myCar.Make = "Buick";
+            myCar.Model = "Encore";
+            myCar.Year = "2019";
+
+            Console.WriteLine($"Year: {myCar.Year}");
+            Console.WriteLine($"Make: {myCar.Make}");
+            Console.WriteLine($"Model: {myCar.Model}");
+        }
+    }
+}
